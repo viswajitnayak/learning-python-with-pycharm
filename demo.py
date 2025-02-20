71024a94ec08e017ec75d88cc1495a3cb0e42916
pragmaticde <codewithviswajit@gmail.com>
2025-02-20 09:53:59 +0530

    Demo files

---
+++ demo.py
@@ -0,0 +1,5 @@
+def print_hi(name):
+    print(f'Hi, {name}')
+    print('How are you doing')
+if __name__ == '__main__':
+    print_hi('PyCHarm')
---
+++ main.py
@@ -0,0 +1,11 @@
+def print_his(name):
+   print(f'Hi, {name}')
+   print('How are you doing today')
+
+def print_names(name):
+   print(f'Hi, {name}')
+   print('How are you do ing today')
+
+if __name__ == '__main__':
+    print_names('PyCharm')
+
